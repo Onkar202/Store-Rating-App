1df7c30798657ccc60a03a1842abd51b7c550ca4
Onkar Kumbhar <85824946+Onkar202@users.noreply.github.com>
2024-09-18 02:49:30 +0530

    Update StoreDashboard.jsx

--- frontend/src/components/StoreDashboard/StoreDashboard.jsx
+++ frontend/src/components/StoreDashboard/StoreDashboard.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './StoreDashboard.css'
+
 
 const StoreDashboard = () =>{
     const [averageRating, setAverageRating] = useState(null);
